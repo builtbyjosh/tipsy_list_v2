3f6a13dfbd5b5fee48bb18c573633f48821e360e
builtbyjosh <pc.refuge@gmail.com>
2021-05-19 12:55:21 -0500

    created reviews controller to handle review creation

---
+++ tipsy_review_backend_v3/app/controllers/reviews_controller.rb
@@ -0,0 +1,23 @@
+class ReviewsController < ApplicationController
+  def index
+    reviews = Review.all
+    render json: reviews
+  end
+
+  def create
+    review = Review.new(review_params)
+    if review.save
+      render json: review
+    else
+      render json: {
+        error: 'Cannot Create',
+        status: 500
+      }, status: 500
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:content, :brewery_id)
+  end
+end
