1f49023972a97faa3fcf9c65eea1862500e0bc5d
builtbyjosh <pc.refuge@gmail.com>
2021-05-19 18:11:28 -0500

    adding css

--- tipsy_review_frontend_v3/src/components/BreweryReview.js
+++ tipsy_review_frontend_v3/src/components/BreweryReview.js
@@ -1,16 +1,39 @@
 import React from "react";
 
-const BreweryReview = ({ reviews }) => {
-  if (!reviews) {
-    return <p>No Reviews Yet</p>;
+const BreweryReview = ({ reviews = [] }) => {
+  if (reviews.length === 0) {
+    return <div>No Reviews Yet</div>;
   }
   return (
-    <div>
-      {reviews.map((r) => {
-        return <p>{r.content}</p>;
-      })}
-    </div>
+<div class="ui relaxed divided list">
+  {reviews.map((r) => {
+    return (
+      <div className="item">
+        <i className="large user middle aligned icon"></i>
+        <div className="content">          
+          <div className="description">{r.content}</div>
+          <button  className="circular ui button negative mini"><i class="close icon"></i> Delete Review</button>
+          
+        </div>
+      </div>
+    );
+  })}
+</div>
   );
 };
 
 export default BreweryReview;
+
+{/* <div class="ui relaxed divided list">
+  {reviews.map((r) => {
+    return (
+      <div class="item">
+        <i class="large github middle aligned icon"></i>
+        <div class="content">
+          <a class="header">Semantic-Org/Semantic-UI</a>
+          <div class="description">Updated 10 mins ago</div>
+        </div>
+      </div>
+    );
+  })}
+</div>; */}
