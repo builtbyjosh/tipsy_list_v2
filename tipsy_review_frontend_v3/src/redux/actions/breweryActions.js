5e73473be7ab5f6648de2cc65d7e1463b701f31c
builtbyjosh <pc.refuge@gmail.com>
2021-05-19 12:38:30 -0500

    added new reducer and action for comments and new breweries

--- tipsy_review_frontend_v3/src/redux/actions/breweryActions.js
+++ tipsy_review_frontend_v3/src/redux/actions/breweryActions.js
@@ -1,7 +1,27 @@
 export const getSavedBreweries = () => {
-  return dispatch => {
-    fetch('http://localhost:3001/breweries')
-    .then(res => res.json())
-    .then((data) => console.log(data))
-  }
+  return (dispatch) => {
+    fetch("http://localhost:3001/breweries")
+      .then((res) => res.json())
+      .then((data) =>
+        dispatch({ type: "FETCH_SAVED_BREWERIES", payload: data })
+      );
+  };
+};
+
+export const createBrewery = (newBreweryData) => {
+  return (dispatch => {
+    fetch('http://localhost:3001/breweries',{
+      method: 'POST',
+      body: JSON.stringify({brewery: newBreweryData})
+    })
+  })
+}
+
+export const createBreweryReview = (newBreweryReview) => {
+  return (dispatch => {
+    fetch('http://localhost:3001/reviews',{
+      method: 'POST',
+      body: JSON.stringify({review: newBreweryReview})
+    })
+  })
 }
--- tipsy_review_frontend_v3/src/redux/reducers/breweryReducer.js
+++ tipsy_review_frontend_v3/src/redux/reducers/breweryReducer.js
@@ -2,6 +2,10 @@ export default (state = [], action) => {
   switch (action.type) {
     case "FETCH_SAVED_BREWERIES":
       return action.payload;
+    case "CREATE_BREWERY":
+      return [...state, action.payload];
+    case "CREATE_REVIEW":
+      return [...state, action.payload];
     default:
       return state;
   }
