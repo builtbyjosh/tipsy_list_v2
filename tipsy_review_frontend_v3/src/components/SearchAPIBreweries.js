09106e2fb8643a0b0d472ba6351106a12e31ee72
builtbyjosh <pc.refuge@gmail.com>
2021-05-19 13:55:14 -0500

    set display of search results

--- tipsy_review_frontend_v3/src/components/SearchAPIBreweries.js
+++ tipsy_review_frontend_v3/src/components/SearchAPIBreweries.js
@@ -1,13 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { getAPIBreweries } from "../redux/actions/breweryActions";
+import BreweryListItem from "./BreweryListItem";
 
 class SearchAPIBreweries extends Component {
+  state = {
+    query: "",
+    breweries: [],
+  };
+
+  componentDidMount() {
+    this.props.getAPIBreweries();
+  }
+
   render() {
     return (
       <div>
-        
+        <h1>Search Results</h1>
+        {this.props.breweries.map((b) => (
+          <BreweryListItem brewery={b} />
+        ))}
       </div>
     );
   }
 }
 
-export default SearchAPIBreweries;
+const mapStateToProps = (state) => {
+  return {
+    breweries: state.breweries,
+  };
+};
+
+export default connect(mapStateToProps, { getAPIBreweries })(
+  SearchAPIBreweries
+);
