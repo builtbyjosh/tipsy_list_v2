1a6a5d4026dd2574aa52e41e3bdd9ee7086c0fba
builtbyjosh <pc.refuge@gmail.com>
2021-05-19 11:49:52 -0500

    created initial components

--- tipsy_review_frontend_v3/src/App.js
+++ tipsy_review_frontend_v3/src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from "react-router-dom";
+
+import Home from './components/Home'
+import BreweryList from './components/BreweryList'
+import AddBreweryForm from './components/AddBreweryForm.js'
+import BreweryShow from './components/BreweryShow'
+
+// / home
+// /breweries - BreweryList
+// /breweries/:id - BreweryShow
+// /brweeries/new - AddBreweryForm
+
 
 function App() {
   return (
     <div >
-      Hello
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/breweries' component={BreweryList} />
+        <Route exact path='/breweries/new' component={AddBreweryForm} />
+        <Route exact path='/breweries/:id' component={BreweryShow} />
+
+      </Switch>
     </div>
   );
 }
