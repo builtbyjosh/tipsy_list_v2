b064828e939f2ff1e062a594974c7f2f48494a85
builtbyjosh <pc.refuge@gmail.com>
2021-05-19 12:02:39 -0500

    setup connect, mapStateToProps, and componentDidMount

--- tipsy_review_frontend_v3/src/components/BreweryList.js
+++ tipsy_review_frontend_v3/src/components/BreweryList.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { getSavedBreweries } from "../redux/actions/breweryActions";
 
 class BreweryList extends Component {
+  componentDidMount() {
+    this.props.getSavedBreweries();
+  }
+
   render() {
-    return (
-      <div>
-        List of breweries
-      </div>
-    );
+    return <div>List of breweries</div>;
   }
 }
 
-export default BreweryList;
+const mapStateToProps = (state) => {
+  return {
+    breweries: state.breweries,
+  };
+};
+
+export default connect(mapStateToProps, { getSavedBreweries })(BreweryList);
