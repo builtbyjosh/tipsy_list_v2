f0880cbe3cb480dcdfb923fc1d853237d23c982d
builtbyjosh <pc.refuge@gmail.com>
2021-05-19 12:36:48 -0500

    initial form setup

--- tipsy_review_frontend_v3/src/components/AddBreweryForm.js
+++ tipsy_review_frontend_v3/src/components/AddBreweryForm.js
@@ -1,10 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class AddBreweryForm extends Component {
+  state = {
+    // name: '',
+    // address: '',
+    // city: '',
+    // state: '',
+    // url: '',
+    content: "",
+    brewery_id: null
+  };
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   render() {
     return (
       <div>
-        Save Brewery
+        <h1>Add New Brewery</h1>
+        <form>
+          <input
+            type="text"
+            name="content"
+            value={this.state.content}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Add Brewery</button>
+        </form>
       </div>
     );
   }
