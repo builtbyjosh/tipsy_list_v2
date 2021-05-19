f87cf1a026434cc9b407219dee2aed9a180f8125
builtbyjosh <pc.refuge@gmail.com>
2021-05-19 12:49:07 -0500

    initial creation of API Search component

---
+++ tipsy_review_frontend_v3/src/components/SearchAPIBreweries.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchAPIBreweries extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default SearchAPIBreweries;
