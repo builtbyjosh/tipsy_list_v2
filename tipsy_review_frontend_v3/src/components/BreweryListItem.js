9a240b80a39779e3a1e25553141cbef12d0fef83
builtbyjosh <pc.refuge@gmail.com>
2021-05-20 10:53:11 -0500

    refactored for css and useHistory for redirect

--- tipsy_review_frontend_v3/src/components/BreweryListItem.js
+++ tipsy_review_frontend_v3/src/components/BreweryListItem.js
@@ -1,11 +1,41 @@
 import React from "react";
-
-// export default function BreweryListItem({ brewery }) {
-//   return <div>{brewery.name}</div>;
-// }
+import {useHistory} from 'react-router-dom'
 
 export default function BreweryListItem({ brewery }) {
-  return <a href={`/breweries/${brewery.id}`}>{brewery.name}</a>;
+  let history = useHistory()
+  let redirect = () => {
+    history.push(`/breweries/${brewery.id}`)
+  }
+  return (
+    <div className="card">
+      <div className="content">
+        <div className="header">{brewery.name}</div>
+      </div>
+      <div className="ui bottom attached button" onClick={redirect}>
+        <i className="add icon"></i>
+        Add New Review
+      </div>
+    </div>
+  );
 }
 
+{
+  /* <a href={`/breweries/${brewery.id}`}>{brewery.name}</a> */
+}
 
+{
+  /* <div className="ui cards">
+  <div className="card">
+    <div className="content">
+      <div className="header">Elliot Fu</div>
+      <div className="description">
+        Elliot Fu is a film-maker from New York.
+      </div>
+    </div>
+    <div className="ui bottom attached button">
+      <i className="add icon"></i>
+      Add New Review
+    </div>
+  </div>  
+</div> */
+}
