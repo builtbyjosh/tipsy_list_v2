dd4a651b59e7354051d0f22f4c18174e5d8db4d4
builtbyjosh <pc.refuge@gmail.com>
2021-05-20 11:51:25 -0500

    added css styling

--- tipsy_review_frontend_v3/src/components/AddReviewForm.js
+++ tipsy_review_frontend_v3/src/components/AddReviewForm.js
@@ -1,29 +1,49 @@
 import React, { Component } from "react";
-import {connect} from 'react-redux'
+import { connect } from "react-redux";
 
-import { createBreweryReview } from '../redux/actions/reviewActions'
+import { createBreweryReview } from "../redux/actions/reviewActions";
 
 class AddReviewForm extends Component {
   state = {
     content: "",
-    brewery_id: this.props.match.params.id
+    brewery_id: this.props.match.params.id,
   };
 
   handleChange = (e) => {
-    this.setState({[e.target.name]: e.target.value});
+    this.setState({ [e.target.name]: e.target.value });
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    this.props.createBreweryReview(this.state, this.props.history)
+    this.props.createBreweryReview(this.state, this.props.history);
   };
 
   render() {
-    
     return (
-      <div>
-        <h1>Add New Review</h1>
-        <form onSubmit={this.handleSubmit}>
+      <div className="ui container">        
+        <form className="ui form" onSubmit={this.handleSubmit}>
+          <div className="field">
+            <label>Review New Review: </label>
+            <input
+              type="text"
+              name="content"
+              value={this.state.content}
+              onChange={this.handleChange}
+            />
+          </div>
+          <button className="ui button" type="submit">
+            Add Review
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default connect(null, { createBreweryReview })(AddReviewForm);
+
+{
+  /* <form onSubmit={this.handleSubmit}>
           <input
             type="text"
             name="content"
@@ -31,10 +51,20 @@ class AddReviewForm extends Component {
             onChange={this.handleChange}
           />
           <button type="submit">Add Review</button>
-        </form>
-      </div>
-    );
-  }
+        </form> */
 }
 
-export default connect(null, {createBreweryReview})(AddReviewForm);
+{
+  /* <form className="ui form" onSubmit={this.handleSubmit}>
+  <div className="field">
+    <label>Review: </label>
+    <input
+            type="text"
+            name="content"
+            value={this.state.content}
+            onChange={this.handleChange}
+          />
+  </div>  
+  <button className="ui button" type="submit">Add Review</button>
+</form> */
+}
