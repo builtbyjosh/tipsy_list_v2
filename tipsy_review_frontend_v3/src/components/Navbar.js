f53714ce60ecedc9113a3db617e9cb24ea8da5d3
builtbyjosh <pc.refuge@gmail.com>
2021-05-20 09:56:40 -0500

    code cleanup

--- tipsy_review_frontend_v3/src/components/Navbar.js
+++ tipsy_review_frontend_v3/src/components/Navbar.js
@@ -4,20 +4,18 @@ class Navbar extends Component {
   state = {};
   render() {
     return (
-
       <div className="ui secondary pointing menu">
         <div className="right menu">
-          <a className=" item" name={"home"} href={'/'}>
-          Home
-        </a>
-        <a className="item" name={"breweries"} href={'/breweries'}>
-          Saved Breweries
-        </a>
-        <a className="item" name={"search"} href={'/search'}>
-          Search New Brewery
-        </a>
+          <a className=" item" name={"home"} href={"/"}>
+            Home
+          </a>
+          <a className="item" name={"breweries"} href={"/breweries"}>
+            Saved Breweries
+          </a>
+          <a className="item" name={"search"} href={"/search"}>
+            Search New Brewery
+          </a>
         </div>
-        
       </div>
     );
   }
