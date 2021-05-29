d6d5b5b9a951cc94dfc176c3e150309d8cd996cd
builtbyjosh <pc.refuge@gmail.com>
2021-05-28 21:04:52 -0500

    removed white space

--- tipsy_review_frontend_v3/src/components/SearchListItems.js
+++ tipsy_review_frontend_v3/src/components/SearchListItems.js
@@ -1,20 +1,7 @@
-// import React from "react";
-
-// export default function SearchListItems({ brewery }) {
-//   return (
-//     <div key={brewery.name}>
-//       <p>{brewery.name}</p>
-//       <button type="submit" onClick={console.log(brewery.name)}>
-//         Save Brewery
-//       </button>
-//     </div>
-//   );
-// }
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
 import { createBrewery } from "../redux/actions/breweryActions";
-
 class SearchListItems extends Component {
   state = {
     name: this.props.brewery.name,
@@ -24,8 +11,7 @@ class SearchListItems extends Component {
     url: this.props.brewery.website_url,
   };
 
-  handleClick = (e) => {
-    
+  handleClick = () => {
     this.props.createBrewery(this.state);
     window.location.href = "/breweries";
   };
@@ -39,7 +25,7 @@ class SearchListItems extends Component {
           </div>
         </div>
         <div className="ui bottom attached button" onClick={this.handleClick}>
-          <i className="plus alternate icon" ></i>
+          <i className="plus alternate icon"></i>
           Add Brewery
         </div>
       </div>
@@ -47,10 +33,3 @@ class SearchListItems extends Component {
   }
 }
 export default connect(null, { createBrewery })(SearchListItems);
-
-// <div>
-//         <p>{this.props.brewery.name}</p>
-//         <button type="submit" onClick={this.handleClick}>
-//           Save Brewery
-//         </button>
-//       </div>
